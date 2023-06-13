c98e908e2bf464ee1965a1fc05d4e3dd6da88835
Tangjj1996 <awesometangji@gmail.com>
2023-06-13 22:28:55 +0800

    feat: Add express cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,32 @@ import 'dotenv/config';
 import express from 'express';
 import chalk from 'chalk';
 import dayjs from 'dayjs';
+import cors from 'cors';
 import config from './config';
 import routes from './route';
 
 const app = express();
 
-app.use(express.static('./assets'));
+/**
+ * Use cors middleware
+ */
+app.use(cors());
+
+/**
+ * Format url-endcode
+ */
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * Format json
+ */
+app.use(express.json());
+
+/**
+ * Global route
+ */
 app.use('/', routes);
 
 app.listen(config.PORT, () => {
-  console.log(chalk.green('✅', `Running success on ${chalk.bgCyanBright(config.PORT)}`, dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss')))
+  console.log(chalk.green('✅', `Running success on ${chalk.bgCyanBright(config.PORT)}`, dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss')));
 })
